ca13a9095400dddc0b124cdea222c9217759f37d
jaffarabbas <gamaportal8@gmail.com>
2023-07-26 09:16:44 +0500

    Update longestPlindrome.js

--- Interview/longestPlindrome.js
+++ Interview/longestPlindrome.js
@@ -1,3 +1,27 @@
-function longestPalin(S){
-    //code here
+function longestPalin(S) {
+  if (!S || S.length === 0) return "";
+
+  let start = 0; // Starting index of the longest palindromic substring found so far
+  let maxLength = 1; // Length of the longest palindromic substring found so far
+
+  // Helper function to expand around the center to find palindromic substrings
+  function expandAroundCenter(left, right) {
+    while (left >= 0 && right < S.length && S[left] === S[right]) {
+      const currentLength = right - left + 1;
+      if (currentLength > maxLength) {
+        maxLength = currentLength;
+        start = left;
+      }
+      left--;
+      right++;
+    }
+  }
+
+  for (let i = 0; i < S.length; i++) {
+    // Consider both odd and even length palindromes
+    expandAroundCenter(i, i); // Odd length
+    expandAroundCenter(i, i + 1); // Even length
+  }
+
+  return S.substring(start, start + maxLength);
 }
