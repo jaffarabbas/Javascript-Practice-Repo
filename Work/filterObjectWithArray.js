65b2aaf066af7b915bf324029792e183f61f6d3a
jaffarabbas <gamaportal8@gmail.com>
2023-08-12 23:09:08 +0500

    Create filterObjectWithArray.js

---
+++ Work/filterObjectWithArray.js
@@ -0,0 +1,27 @@
+const obj = {
+    main:[{
+        o:"Jaffar",
+    },{
+        o:"Zain",
+    },{
+        o:"Ahmed",
+    },{
+        o:"Fahad",
+    }]
+};
+
+const arr = ["Jaffar","Zain"]
+
+const filtered = obj.main.filter((item) => {
+    return arr.includes(item.o)
+})
+
+function filterObjectWithArray(obj, arr) {
+    const filtered = obj.main.filter((item) => {
+        return arr.includes(item.o)
+    })
+    return filtered
+}
+
+console.log(filtered)
+console.log(filterObjectWithArray(obj, arr))
