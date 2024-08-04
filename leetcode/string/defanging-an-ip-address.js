6d5897317636cd85c8bb609fe2908766754de9ba
jaffarabbas <gamaportal8@gmail.com>
2024-08-04 13:52:38 +0500

    Commited File : defanging-an-ip-address.js at 04/08/2024 1:52:38 pm

---
+++ leetcode/string/defanging-an-ip-address.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    return address.split('.').join('[.]')
+};
+
+console.log(defangIPaddr('1.1.1.1'))
