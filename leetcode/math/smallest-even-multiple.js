75abf3cb5c34717a70baf9be8ec39c4568e50557
jaffarabbas <gamaportal8@gmail.com>
2024-08-05 22:41:48 +0500

    Create smallest-even-multiple.js

---
+++ leetcode/math/smallest-even-multiple.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+    if (n % 2 === 0) {
+        return n; 
+      }
+    return n * 2; 
+};
+
+console.log(smallestEvenMultiple(5))
