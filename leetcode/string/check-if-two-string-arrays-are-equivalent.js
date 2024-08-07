63688e01ccebe2c21fcf797a1a40b9b9a42c4efd
jaffarabbas <gamaportal8@gmail.com>
2024-08-07 22:07:07 +0500

    Create check-if-two-string-arrays-are-equivalent.js

---
+++ leetcode/string/check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    return word1.join("") === word2.join("")
+};
+
+console.log(arrayStringsAreEqual(["abc", "d", "defg"],["abcddefg"]))
