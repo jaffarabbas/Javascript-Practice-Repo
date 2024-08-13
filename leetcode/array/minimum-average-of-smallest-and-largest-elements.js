02e5986041f85f57cc6ded149a767b1849d581a1
jaffarabbas <gamaportal8@gmail.com>
2024-08-13 22:20:49 +0500

    Update minimum-average-of-smallest-and-largest-elements.js

--- leetcode/array/minimum-average-of-smallest-and-largest-elements.js
+++ leetcode/array/minimum-average-of-smallest-and-largest-elements.js
@@ -3,20 +3,16 @@
  * @return {number}
  */
 var minimumAverage = function(nums) {
-    let avg = []
-    while(nums.length > 0) {
-        let max = Math.max(...nums)
-        let min = Math.min(...nums)
-        let cal = (max + min) / 2;
-        avg.push(cal)
-        if (nums.indexOf(max) !== -1) {
-            nums.splice(nums.indexOf(max), 1);
+        let avg = []
+        while(nums.length > 0) {
+            let max = Math.max(...nums)
+            let min = Math.min(...nums)
+            let cal = (max + min) / 2;
+            avg.push(cal)
+            nums.indexOf(max) !== -1 && nums.splice(nums.indexOf(max), 1);
+            nums.indexOf(min) !== -1 && nums.splice(nums.indexOf(min), 1);
         }
-        if (nums.indexOf(min) !== -1) {
-            nums.splice(nums.indexOf(min), 1);
-        }
-    }
-    return Math.min(...avg)
+        return Math.min(...avg)
 };
 
 console.log(minimumAverage([7,8,3,4,15,13,4,1]))
