91b36bde137a82d674e1bf423faaa33c6ebbfbbd
jaffarabbas <gamaportal8@gmail.com>
2024-08-19 21:23:43 +0500

    Update replace-all-digits-with-characters.js

--- leetcode/string/replace-all-digits-with-characters.js
+++ leetcode/string/replace-all-digits-with-characters.js
@@ -4,15 +4,13 @@
  */
 var replaceDigits = function(s) {
     let result = "";
-        let prevChar = "";  // Keeps track of the previous character
+        let prevChar = "";  
     
         for (const char of s) {
             if (char >= 'a' && char <= 'z') {
-                // Update the previous character
                 prevChar = char;
                 result += char;
             } else if (char >= '0' && char <= '9') {
-                // Calculate the new character
                 const shift = Number(char);
                 const newChar = String.fromCharCode(prevChar.charCodeAt(0) + shift);
                 result += newChar;
