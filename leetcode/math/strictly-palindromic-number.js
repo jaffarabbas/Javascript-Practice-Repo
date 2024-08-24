a01ea153a3401e3fdb57ada13da01ad8a36a5d8e
jaffarabbas <gamaportal8@gmail.com>
2024-08-24 12:30:19 +0500

    Create strictly-palindromic-number.js

---
+++ leetcode/math/strictly-palindromic-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isStrictlyPalindromic = function(n) {
+    let endBase = n - 2;
+    let f = false;
+    for (let i = 2; i <= endBase; i++) {
+        let a = n.toString(i);
+        let left = 0,right = a.length - 1;
+        while (left < right) {
+            if (a[left] !== a[right]) {
+                return false
+            }
+            left++; 
+            right--; 
+        }
+    }
+    return true;
+};
+
+console.log(isStrictlyPalindromic(9))
