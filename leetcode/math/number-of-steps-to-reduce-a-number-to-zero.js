582e3cc44ae8c8dd195539e5d4bd7a029f0afd1e
jaffarabbas <gamaportal8@gmail.com>
2024-08-27 21:46:09 +0500

    Create number-of-steps-to-reduce-a-number-to-zero.js

---
+++ leetcode/math/number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let count = 0;
+    while(num > 0){
+        if(num %2 == 0){
+            num = num / 2;
+        }else{
+            num = num - 1;
+        }
+        count++;
+    }
+    return count;
+};
+
+console.log(numberOfSteps(14))
